7b0cc78011ad1efc13456062da15478a5ad2a2cb
Adam Foster <adam.foster@drivvn.com>
2026-04-03 08:21:54 +0100

    Add ECR repository policy and validation steps
    - Add ECR repository policy to allow GitHub Actions to push images
    - Grant Lambda service permission to pull images
    - Grant GitHub Actions app role permission to push images
    - Add terraform fmt and validate steps to apply workflow
    - Fixes HTTP 403 error in CI/CD pipeline
    
    This resolves the issue where GitHub Actions couldn't push Docker
    images to ECR due to missing repository permissions.

--- ecr.tf
+++ ecr.tf
@@ -40,3 +40,48 @@ resource "aws_ecr_lifecycle_policy" "lambda_container" {
     ]
   })
 }
+
+resource "aws_ecr_repository_policy" "lambda_container" {
+  repository = aws_ecr_repository.lambda_container.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:SetRepositoryPolicy",
+          "ecr:DeleteRepositoryPolicy",
+          "ecr:GetRepositoryPolicy"
+        ]
+        Condition = {
+          StringLike = {
+            "aws:sourceArn" = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:*"
+          }
+        }
+      },
+      {
+        Sid    = "GitHubActionsECRPushPolicy"
+        Effect = "Allow"
+        Principal = {
+          AWS = local.enable_github_actions ? aws_iam_role.github_actions_app[0].arn : data.aws_caller_identity.current.arn
+        }
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
